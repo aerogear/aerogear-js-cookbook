779dbc78b2d428f7fa6dc6a82dfeb2b00e4c3476
Lukas Fryc <lfryc@redhat.com>
2014-05-02 12:43:25 +0200

    working angular integration

--- vertx-notifier/app/scripts/app.js
+++ vertx-notifier/app/scripts/app.js
@@ -1,6 +1,33 @@
 /*global define */
-define([], function () {
+define(['angular'], function (app) {
     'use strict';
 
-    return '\'Allo \'Allo!';
+    return angular.module('app', [])
+
+        .controller('SendController', function($scope) {
+            $scope.name = 'Mr. Yoda';
+            $scope.message = 'May Force be with You';
+
+            $scope.sendMessage = function(name, message) {
+                var message = {
+                    name: name,
+                    text: message
+                };
+                $scope.$emit('message', message);
+            }
+        })
+
+        .controller('ListController', function($scope, $rootScope) {
+            $scope.messages = [];
+
+            $rootScope.$on('message', function(event, message) {
+                $scope.messages.push(message);
+            });
+        })
+
+        .filter('reverse', function() {
+            return function(items) {
+                return items.slice().reverse();
+            };
+        });
 });
--- vertx-notifier/app/scripts/main.js
+++ vertx-notifier/app/scripts/main.js
@@ -5,16 +5,25 @@ require.config({
     paths: {
         jquery: '../bower_components/jquery/dist/jquery',
         bootstrap: '../bower_components/bootstrap/dist/js/bootstrap',
-        fastclick: '../bower_components/fastclick/lib/fastclick'
+        fastclick: '../bower_components/fastclick/lib/fastclick',
+        angular: '../bower_components/angular/angular'
     },
     shim: {
         bootstrap: {
             deps: ['jquery'],
             exports: 'jquery'
+        },
+        angular: {
+            deps: ['jquery'],
+            init: function() {
+                return this.angular;
+            }
         }
     }
 });
 
-require(['app', 'bootstrap', 'fastclick.init'], function () {
+require(['angular', 'app', 'bootstrap', 'fastclick.init'], function () {
     'use strict';
+
+    angular.bootstrap(document, ['app']);
 });
