7793caa1b24907eba998aab213cf6bec05f857a5
Lukas Fryc <lfryc@redhat.com>
2014-05-08 21:18:50 +0200

    AGJS-41: finished sample and readme for vert.x notifier

--- vertx-notifier/app/scripts/app.js
+++ vertx-notifier/app/scripts/app.js
@@ -4,38 +4,63 @@ define(['angular', 'aerogear'], function (app, AeroGear) {
 
     return angular.module('app', [])
 
-        .controller('SendController', function($scope, messenger) {
-            $scope.name = 'Mr. Yoda';
-            $scope.message = 'May Force be with You';
+        .controller('SendController', function($scope, chatBus) {
+            $scope.name = localStorage.name;
+            $scope.message = '';
 
-            $scope.sendMessage = function(name, message) {
+            $scope.sendMessage = function() {
                 var message = {
-                    name: name,
-                    text: message
+                    name: $scope.name,
+                    text: $scope.message
                 };
-                $scope.$emit('message', message);
-            }
-        })
+                // send message to all subscribers
+                chatBus.send('chat.messages', message, true);
+                $scope.message = '';
+            };
 
-        .controller('ListController', function($scope, $rootScope) {
-            $scope.messages = [];
-
-            $rootScope.$on('message', function(event, message) {
-                $scope.messages.push(message);
+            // watches changes of user name and saves it to localStorage
+            $scope.$watch('name', function() {
+               localStorage.name = $scope.name;
             });
         })
 
-        .factory('messenger', function() {
-            return AeroGear.Notifier({
-                name: 'messages',
-                type: 'vertx',
-                settings: {
-                    autoConnect: true,
-                    connectURL: 'localhost:9000'
-                }
-            })
+        // create a controller for displaying list of messages
+        .controller('ListController', function($scope, messageList) {
+            $scope.messages = messageList;
         })
 
+        // initialize list of messages
+        .factory('messageList', function() {
+            return [];
+        })
+
+        // create a chat service (injects global list of messages)
+        .factory('chatBus', function(messageList, $rootScope) {
+            var chatBus = AeroGear.Notifier({
+                name: 'chat',
+                type: 'vertx',
+                settings: {
+                    // specify address of the vert.x sockjs bridge
+                    connectURL: 'http://localhost:8080/eventbus',
+                    autoConnect: true,
+                    onConnect: function() {
+                        // subscribe to the channel
+                        chatBus.subscribe({
+                            address: "chat.messages",
+                            callback: function(message) {
+                                // update list of messages
+                                messageList.push(message);
+                                // we have to notify angular about change to the model (messageList update)
+                                $rootScope.$apply();
+                            }
+                        });
+                    }
+                }
+            }).clients.chat;
+            return chatBus;
+        })
+
+        // used for reverting a message list when displayed (newest messages on the top)
         .filter('reverse', function() {
             return function(items) {
                 return items.slice().reverse();
--- vertx-notifier/server.js
+++
@@ -1,5 +0,0 @@
-load('vertx.js');
-
-vertx.createHttpServer().requestHandler(function(req) {
-    req.response.end("Hello World!");
-}).listen(8080, 'localhost');
