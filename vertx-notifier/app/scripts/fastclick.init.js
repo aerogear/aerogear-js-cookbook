45ddf08b5cef9d23b3d2854193023c5f46b5a88a
Lukas Fryc <lfryc@redhat.com>
2014-05-13 23:22:42 +0200

    added empty lines on the end of HTML and JS files

--- vertx-notifier/app/scripts/fastclick.init.js
+++ vertx-notifier/app/scripts/fastclick.init.js
@@ -6,14 +6,3 @@ define(['jquery', 'fastclick'], function ($, FastClick) {
         FastClick.attach(document.body);
     });
 });
-
-
-/*
-Don't forget to add a shim for addEventListener if you want to support IE8 and below.
-
-Otherwise, if you're using jQuery:
-
-$(function() {
-    FastClick.attach(document.body);
-});
-*/
